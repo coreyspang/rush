9bae5c64b9b1c22cd90c89fcc2f29240816fc380
Corey Spangler <corey@therushstudio.com>
2013-04-12 07:50:39 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rush::Application.config.secret_token = 'd3ee2cbec97be021e5fb0e8ecc2780f126f398f371c6fe1b130254efd74090b6620d01b54d78c3f46507de0887de56cec3257f1b8a195c317626b5764fa42c18'
